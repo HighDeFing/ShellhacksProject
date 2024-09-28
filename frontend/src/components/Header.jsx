4d67f7f6968dbe77b72e291dd72582789df0a6ba
6uan <6uannn@gmail.com>
2024-09-28 14:52:38 -0400

    feat: added test logic to fetch tutors inside components

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -13,9 +13,11 @@ const Header = () => {
           <button className="flex h-12 items-center justify-center rounded-md border-2 border-black px-4 py-2 text-2xl hover:bg-gray-700 hover:text-white">
             I am a Student
           </button>
-          <button className="flex h-12 items-center justify-center rounded-md border-2 border-black px-4 py-2 text-2xl hover:bg-gray-700 hover:text-white">
-            I am a Tutor
-          </button>
+          <Link to="/tutors">
+            <button className="flex h-12 items-center justify-center rounded-md border-2 border-black px-4 py-2 text-2xl hover:bg-gray-700 hover:text-white">
+              I am a Tutor
+            </button>
+          </Link>
         </div>
       </div>
     </div>
