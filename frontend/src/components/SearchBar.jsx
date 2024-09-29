8873da51b357d855efaa00d007ccb69532f3cc16
6uan <6uannn@gmail.com>
2024-09-28 21:19:06 -0400

    fix: search bar was overflowing into buttons

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -2,7 +2,7 @@ import { FaSearch } from "react-icons/fa";
 
 const SearchBar = () => {
   return (
-    <div className="sticky top-0 z-50 flex min-h-24 w-full items-center justify-center">
+    <div className="sticky top-0 z-0 mx-auto flex min-h-24 w-1/2 items-center justify-center">
       <div className="box-border flex h-14 w-[860px] items-center justify-center rounded-lg border-2 border-black bg-white p-4">
         <label className="sr-only">Course Search</label>
         <input
