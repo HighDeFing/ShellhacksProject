f9a80703f86eb2513a4ccef433c29e4e04559514
6uan <6uannn@gmail.com>
2024-09-29 04:19:18 -0400

    feat: if no results found tell user to try another query

--- frontend/src/components/CardContainer.jsx
+++ frontend/src/components/CardContainer.jsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import CourseCard from "./CourseCard";
 import SubjectSelection from "./SubjectSelection";
 import { useState, useEffect } from "react";
+import { FaSearch } from "react-icons/fa";
 
 const CardContainer = ({ searchQuery }) => {
   const [courses, setCourses] = useState([]); // Stores all courses
@@ -32,18 +33,31 @@ const CardContainer = ({ searchQuery }) => {
         {/* Pass the subject selection handler */}
         <SubjectSelection setSelectedSubject={setSelectedSubject} />
       </div>
+
       <div className="mx-auto w-full max-w-screen-xl">
-        {/* Grid for displaying course cards */}
-        <div className="grid w-full grid-cols-5 place-content-center gap-6 px-1 pt-5">
-          {filteredCourses.map((course) => (
-            <Link to={`/course/${course._id}`} key={course._id}>
-              <CourseCard
-                className="flex w-full items-center justify-center"
-                course={course}
-              />
-            </Link>
-          ))}
-        </div>
+        {/* Check if no courses or if there are few results */}
+        {filteredCourses.length === 0 ? (
+          <div className="flex h-96 flex-col items-center justify-center">
+            <FaSearch className="mr-2 text-6xl text-gray-400" />
+            <p className="font-newfrank mt-4 text-xl font-bold uppercase text-gray-600">
+              No Results Found
+            </p>
+            <p className="font-newfrank mt-1 text-sm font-light text-gray-600">
+              Please try another query
+            </p>
+          </div>
+        ) : (
+          <div className="grid min-h-[400px] w-full grid-cols-5 place-content-center gap-6 px-1 pb-7 pt-7">
+            {filteredCourses.map((course) => (
+              <Link to={`/course/${course._id}`} key={course._id}>
+                <CourseCard
+                  className="flex w-full items-center justify-center"
+                  course={course}
+                />
+              </Link>
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
