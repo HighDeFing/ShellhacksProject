19cb50349468078a0b75f1c28b19a9fff50510a3
6uan <6uannn@gmail.com>
2024-09-28 23:31:19 -0400

    feat(db): updated courses data

--- frontend/src/components/CourseCard.jsx
+++ frontend/src/components/CourseCard.jsx
@@ -5,7 +5,7 @@ const CourseCard = ({ course: course, className }) => {
     <div className={twMerge("card h-48 rounded-md", className)}>
       <div className="flex h-full w-full flex-col items-center justify-center rounded-md border-2 border-black align-middle">
         <h5>{course.name}</h5>
-        <p>{course.tutors_id}</p>
+        {/* <p>{course.description}</p> */}
         <p>View Tutors</p>
       </div>
     </div>
